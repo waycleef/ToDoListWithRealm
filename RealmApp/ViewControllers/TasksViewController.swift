ef3dfbfcaf3afad843c4a65573cdfdf71cf50017
waycleef <khmalikov@gmail.com>
2022-12-30 10:43:24 +0600

    Добавление метода didSelect для отмены веделения секции

--- RealmApp/ViewControllers/TasksViewController.swift
+++ RealmApp/ViewControllers/TasksViewController.swift
@@ -81,6 +81,10 @@ class TasksViewController: UITableViewController {
         return UISwipeActionsConfiguration(actions: [doneAction, editAction, deleteAction])
     }
     
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
     
     
     @objc private func addButtonPressed() {
